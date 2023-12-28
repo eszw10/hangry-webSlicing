93db6dae8925e15f77e5fcf17a09f228156ff688
eryc_s.z.w <erycszw10@gmail.com>
2023-12-28 13:19:27 +0700

    finished project

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ const Card = ({data}) => {
             <div className="absolute top-4 left-4 xl:top-6 xl:left-6 bg-white h-10 w-10 xl:h-20 xl:w-20 rounded-full flex items-center justify-center">
                 <img src={data.logo} alt={`logo ${data.title}`} className="xl:h-12 h-6"/>
             </div>
-            <img src={data.image} alt={`${data.title}`} className="xl:h-[350px] md:w-[350px] h-[167px] rounded-xl" />
+            <img src={data.image} alt={`${data.title}`} className="xl:h-[350px] xl:w-[350px] md:h-56 h-[167px] rounded-xl" />
             <div className="flex flex-col gap-1 xl:gap-2">
                 <div className="flex items-center font-bold xl:text-3xl text-sm gap-2 cursor-pointer">
                     <p className="peer">{data.title}</p>
